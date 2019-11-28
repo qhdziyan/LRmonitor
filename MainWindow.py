0fbc41542d95a8ed43db4a6da4907412c6b6164b
qhdzyian <qhdziyan@126.com>
2019-11-28 15:19:39 +0800

    更新

--- MainWindow.py
+++ MainWindow.py
@@ -99,7 +99,6 @@ if __name__ == "__main__":
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     mainwindow = Main()
     mainwindow.showMaximized()
-    print(123)
     sys.exit(app.exec_())
 
 
