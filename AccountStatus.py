1ce20cf4899f3736364f6c2e9670a8c03a25c3d4
qhdziyan <qhfziyan@126.com>
2019-11-29 10:35:13 +0800

    更新

--- AccountStatus.py
+++ AccountStatus.py
@@ -20,23 +20,21 @@ class myform(QWidget, Ui_Form):
     def load(self):
         db = pymysql.connect("localhost", "root", "Lianrun!@#", "posorders")
         cursor = db.cursor()
-        cursor.execute("SELECT * from account.accountstatus order")
+        cursor.execute("SELECT * from account.accountstatus")
         data = cursor.fetchall()
         row = cursor.rowcount  # 取得记录个数，用于设置表格的行数
-        vol = len(row[0])  # 取得字段数，用于设置表格的列数
+        vol = len(data[0])  # 取得字段数，用于设置表格的列数
         cursor.close()
         db.close()
-
-
 
         self.tableWidget.setRowCount(row)
         self.tableWidget.setColumnCount(vol)
 
         for i in range(row):
             for j in range(vol):
-                temp_data = row[i][j]  # 临时记录，不能直接插入表格
-                data = QTableWidgetItem(str(temp_data))  # 转换后可插入表格
-                self.tableWidget.setItem(i, j, data)
+                temp_data = data[i][j]  # 临时记录，不能直接插入表格
+                showdata = QTableWidgetItem(str(temp_data))  # 转换后可插入表格
+                self.tableWidget.setItem(i, j, showdata)
 
 
 app = QApplication(sys.argv)
