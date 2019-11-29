cdff466a4e3c114d73db61af7f7925906d6be398
qhdziyan <qhfziyan@126.com>
2019-11-29 10:45:57 +0800

    更新

--- AccountStatus.py
+++ AccountStatus.py
@@ -1,6 +1,9 @@
 import sys
+import time
 from AccountStatusUI import Ui_Form
-from PyQt5.Qt import QWidget, QApplication, QTableWidgetItem
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 import pymysql
 
 
@@ -9,14 +12,20 @@ class myform(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
         self.pushButton_2.clicked.connect(self.clear)
-        self.pushButton.clicked.connect(self.load)
+        self.threadstartslot()
         self.show()
-
+    # 线程测试开始
+    def threadstartslot(self):
+        self.work = Thread()
+        self.work.trigger.connect(self.load)  # 线程中的trigger与主类中的方法进行绑定
+        self.work.start()  # 开启线程
+    # 线程测试停止
+    def threadstopslot(self):
+        self.work.threadstartflag = False
     def clear(self):
         pass
-
+    # 更新UI方法
     def load(self):
         db = pymysql.connect("localhost", "root", "Lianrun!@#", "posorders")
         cursor = db.cursor()
@@ -35,7 +44,19 @@ class myform(QWidget, Ui_Form):
                 temp_data = data[i][j]  # 临时记录，不能直接插入表格
                 showdata = QTableWidgetItem(str(temp_data))  # 转换后可插入表格
                 self.tableWidget.setItem(i, j, showdata)
-
+class Thread(QThread):
+    trigger = pyqtSignal(str)#注意pyqtSignal一定要实例到__init__前面
+    def __init__(self):
+        super(Thread, self).__init__()
+        #定义的变量
+        self.threadstartflag=True
+        self.timecount=0
+    #执行耗时操作
+    def run(self):
+        while self.threadstartflag == True:
+            self.trigger.emit(u"计时%d"%self.timecount)#发送更新GUI的信号
+            self.timecount+=1
+            time.sleep(1)
 
 app = QApplication(sys.argv)
 w = myform()
